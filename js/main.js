b2126c32c165e4add394f64c17c09e723c111518
Fernando Hurtado <fernandohur@gmail.com>
2018-02-05 21:10:39 +0100

    changes on Mon Feb  5 21:10:39 CET 2018

--- js/main.js
+++ js/main.js
@@ -3,25 +3,44 @@ window.sr = ScrollReveal();
 sr.reveal('.container-fluid');
 
 // Countdown
-var countdown = new Countdown({
-    selector: '#timer',
-    msgAfter: "Congratulations!",
-    leadingZeros: true,
-    msgPattern: "{days} дней",
-    dateEnd: new Date('2018/05/26 00:00')
-    // onEnd: countdownEnd()
-});
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
-// function countdownEnd() {
-// 	return alert('countdownEnd');
-// }
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
 
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
 
-// var myMap = document.getElementById('map');
-// myMap.behaviors.disable('drag');
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-// Just to down
-// setTimeout(window.scrollTo(0,document.body.scrollHeight), 1000);
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
 
-// setTimeout((var mapFirstTag = document.getElementsByTagName('ymaps')[0]), 4000);
-// setTimeout((mapFirstTag.style = ""), 6000);
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date('2018/05/26 00:00');
+initializeClock('clockdiv', deadline);
